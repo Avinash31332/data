c01dce3dff00b22a91ecdcc5d9bb992589ec8f32
Avinash <avinashviolin4@gmail.com>
2024-12-07 01:07:54 +0530

    input ejs

--- config/db.js
+++ config/db.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // Remove useNewUrlParser and useUnifiedTopology
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Database connected successfully');
   } catch (error) {
     console.error('Database connection failed', error);
